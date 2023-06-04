81a1515b74ed588ef48e1b062d45037315f4a3e0
malaikaikartel <malaikaikartel123@gmail.com>
2023-06-04 15:40:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+// import { useRoute } from 'vue-router'
+import TheSidebar from './components/TheSidebar'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { initWallet } from 'solana-wallets-vue'
+import { initWorkspace } from '@/composables'
+
+import { WalletMultiButton} from 'solana-wallets-vue'
+
+
+
+// const route = useRoute()
+
+const heading = "SolLinked"
+
+const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+]
+
+initWallet({ wallets, autoConnect: true })
+initWorkspace()
+
+
+</script>
+
+<template>
+    <div class="e-card e-card-horizontal">
+
+        <!-- Sidebar. -->
+        <the-sidebar class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"></the-sidebar>
+
+        <!-- Main -->
+        <main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
+            <header class="flex space-x-6 items-center justify-between px-8 py-4 border-b">
+                <div class="text-xl text-blue-700 font-bold w-full focus:outline-none resize-none mb-3" v-text="heading"></div>
+                <div class="text-xl text-blue-700 font-bold">
+                    <wallet-multi-button></wallet-multi-button>
+                </div>
+            </header>
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
---
+++ app/src/api/index.js
@@ -0,0 +1,4 @@
+export * from './fetch-posts'
+export * from './get-post'
+export * from './send-post'
+export * from './update-post'
